78a5ddce0ddb1f37b4c2605c1bd12da8e366e8cb
Marcin Chalecki <alcaster0@gmail.com>
2016-11-24 12:00:01 +0100

    Code cleaning. Catching exceptions.

--- src/java_project_programming3/TCPClientPac/TCPClient.java
+++ src/java_project_programming3/TCPClientPac/TCPClient.java
@@ -1,10 +1,21 @@
 package java_project_programming3.TCPClientPac;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.concurrent.TimeUnit;
 
 class TCPClient {
+    private static boolean exit;
+    private static BufferedReader inFromUser;
+
+    private TCPClient() {
+        exit = false;
+        inFromUser = new BufferedReader(new InputStreamReader(System.in));
+    }
+
     private static Socket make_connection() {
         Socket clientSocket = null;
         try {
@@ -16,47 +27,58 @@ class TCPClient {
         return clientSocket;
     }
 
-    private static boolean ping(Socket clientSocket) {
-        //Doesn't work due to TODO.2
-        try {
-            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-            outToServer.writeBytes("Ping\n");
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
     public static void main(String argv[]) throws IOException, InterruptedException {
-        run();
+        TCPClient abc = new TCPClient();
+        abc.run();
     }
 
-    private static void run() throws IOException, InterruptedException {
-        String send_text;
-        String received_text="";
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        boolean exit = false;
+    private void run() throws InterruptedException {
+        String received_text;
+        wholeServer:
         while (true) {
             TimeUnit.SECONDS.sleep(1);
             System.out.println("Making new connection");
             Socket clientSocket = make_connection();
-
             while (clientSocket != null) {
-                send_text = inFromUser.readLine();
-                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                outToServer.writeBytes(send_text + '\n');
-                received_text = inFromServer.readLine();
-                System.out.println("FROM SERVER: " + received_text);
-
-                if (exit) {
-                    clientSocket.close();
+                try {
+                    sendMessageFromSystemInput(clientSocket);
+                } catch (IOException e) {
+                    System.out.println("Cant't send message");
                     break;
+                }
+                try {
+                    received_text = getMessageFromServer(clientSocket);
+                } catch (IOException e) {
+                    System.out.println("Cant receive a message");
+                    break;
+                }
+                System.out.println("FROM SERVER: " + received_text);
+                if (exit) {
+                    try {
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.getStackTrace();
+                    }
+                    break wholeServer;
                 }
             }
             System.out.println("Connection broken");
         }
 
+    }
+
+    private static String getMessageFromServer(Socket clientSocket) throws IOException {
+        String received_text;
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        received_text = inFromServer.readLine();
+        return received_text;
+
+    }
+
+    private static void sendMessageFromSystemInput(Socket clientSocket) throws IOException {
+        String send_text;
+        send_text = inFromUser.readLine();
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        outToServer.writeBytes(send_text + '\n');
     }
 }
--- src/java_project_programming3/TCPServerPac/ClientThread.java
+++ src/java_project_programming3/TCPServerPac/ClientThread.java
@@ -5,35 +5,62 @@ import java.net.Socket;
 
 public class ClientThread extends Thread {
     private Socket socket;
+    private BufferedReader brinp = null;
+    private DataOutputStream out = null;
 
     ClientThread(Socket clientSocket) {
         System.out.println("New client");
-        this.socket = clientSocket;
-    }
-
-    public void run() {
-        InputStream inp = null;
-        BufferedReader brinp = null;
-        DataOutputStream out = null;
+        socket = clientSocket;
         try {
-            inp = socket.getInputStream();
+            InputStream inp = socket.getInputStream();
             brinp = new BufferedReader(new InputStreamReader(inp));
             out = new DataOutputStream(socket.getOutputStream());
         } catch (IOException e) {
-            return;
+            System.out.println("Cannot initialize socket");
+            e.printStackTrace();
         }
+    }
+
+    private int sendBack(String message) {
+        try {
+            out.writeBytes(message + "\n\r");
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return 1;
+    }
+
+    private int ping() throws IOException {
+        sendBack("PING");
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String received_text = inFromServer.readLine();
+        if (received_text.equals("PONG"))
+            return 1;
+        else return 0;
+    }
+
+    private void interpretMessage(String message) {
+        if (message.equals("PING")) {
+            sendBack("PONG");
+        } else {
+            sendBack(message);
+        }
+    }
+
+    public void run() {
         String line;
         while (true) {
             try {
                 line = brinp.readLine();
-                System.out.println(line);
                 if ((line == null) || line.equalsIgnoreCase("QUIT")) {
                     System.out.println("Client has disconnected");
                     socket.close();
                     return;
                 } else {
-                    out.writeBytes(line + "\n\r");
-                    out.flush();
+                    System.out.println(line);
+                    interpretMessage(line);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -41,5 +68,4 @@ public class ClientThread extends Thread {
             }
         }
     }
-
 }
