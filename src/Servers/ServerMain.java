fd82d73d021b2626de59b35a232da6409cf6a59a
Marcin Chalecki <alcaster0@gmail.com>
2016-12-14 01:04:34 +0100

    Adding users to map when they connect first time. Deleting users that disconnect. Order of methods. Update todo.

--- src/Servers/ServerMain.java
+++ src/Servers/ServerMain.java
@@ -13,11 +13,48 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
 public class ServerMain {
     private final int PORT = 6789;
     private String temp_host = "127.0.0.1";
-    private String abc = "";
     private String nextHost = "127.0.0.11";
+    private BiMap<String, String> users = HashBiMap.create();
+
+
+    public static void main(String args[]) {
+        ServerMain srv = new ServerMain();
+        srv.run();
+    }
+
+    private void run() {
+        String received_text;
+        ServerSocket serverSocket = createServer();
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+            }
+            Socket connectionSocket = makeConnectionSocket(serverSocket);
+            received_text = null;
+            try {
+                BufferedReader inFromClient =
+                        new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                received_text = inFromClient.readLine();
+            } catch (IOException e) {
+                System.out.println("IO_exception");
+            }
+            if (received_text != null) {
+                System.out.println("Received: " + received_text);
+                processMessage(received_text);
+            } else {
+                System.out.print("Client has disconnected");
+                break;
+            }
+        }
+    }
 
     @Nullable
     private ServerSocket createServer() {
@@ -28,7 +65,6 @@ public class ServerMain {
             System.out.println("Can't create inet address");
         }
         try {
-
             ServerSocket serverSocket = new ServerSocket(PORT, 50, address);
             System.out.println("Created server");
             return serverSocket;
@@ -62,6 +98,36 @@ public class ServerMain {
         return clientSocket;
     }
 
+    private void processMessage(String message) {
+        org.json.JSONObject obj = new org.json.JSONObject(message);
+        int type = obj.getInt("type");
+        switch (type) {
+            case 1:
+                addUser(obj);
+                break;
+            case 2:
+                deleteUser(obj);
+                break;
+            case 3:
+                sendMessage(nextHost, message);
+                break;
+        }
+    }
+
+    private void addUser(org.json.JSONObject message) {
+        org.json.JSONObject content = new org.json.JSONObject(message.get("content").toString());
+        String nick = content.getString("nick");
+        String IP = message.getString("IP_from");
+        String entry = users.get(nick);
+        if (entry == null) users.put(nick, IP);
+    }
+
+    private void deleteUser(org.json.JSONObject message) {
+        String IP = message.getString("IP_from");
+        String entry = users.inverse().get(IP);
+        if (entry != null) users.inverse().remove(IP);
+    }
+
     private void sendMessage(String host, String message) {
         System.out.println("Making new connection");
         Socket clientSocket = makeSenderSocket(host);
@@ -71,39 +137,6 @@ public class ServerMain {
                 outToServer.writeBytes(message + '\n');
             } catch (IOException e) {
                 System.out.println("Can't send message");
-            }
-        }
-    }
-
-    public static void main(String args[]) {
-        ServerMain srv = new ServerMain();
-        srv.run();
-    }
-
-    private void run() {
-        String received_text;
-        ServerSocket serverSocket = createServer();
-        while (true) {
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                System.out.println("Interrupted");
-            }
-            Socket connectionSocket = makeConnectionSocket(serverSocket);
-            received_text = null;
-            try {
-                BufferedReader inFromClient =
-                        new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                received_text = inFromClient.readLine();
-            } catch (IOException e) {
-                System.out.println("IO_exception");
-            }
-            if (received_text != null) {
-                System.out.println("Received: " + received_text);
-                sendMessage(nextHost, received_text);
-            } else {
-                System.out.print("Client has disconnected");
-                break;
             }
         }
     }
