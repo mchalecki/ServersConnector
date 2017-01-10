d4632631ca06aeded56f0eecf250220258e117d7
UlaBorys <ula_borys@op.pl>
2017-01-10 12:23:55 +0100

    Validating address

--- Client/src/Client/GUI/FirstFrame.java
+++ Client/src/Client/GUI/FirstFrame.java
@@ -26,6 +26,7 @@ public class FirstFrame extends JFrame {
 
     private String Nick;
     private String Address;
+    private boolean verified;
 
     public FirstFrame() {
         JPanel centerPanel = new JPanel();
@@ -41,46 +42,45 @@ public class FirstFrame extends JFrame {
         NickBox = new JTextField();
         AddressBox = new JTextField();
 
-        NickBox.addKeyListener(new EnterListener());
-        AddressBox.addKeyListener(new EnterListener());
+        AddressBox.addKeyListener(new InputListener());
 
         EnterButton = new JButton("Enter");
-        EnterButton.addActionListener(new EnterListener());
+        EnterButton.addActionListener(new InputListener());
 
         GridBagConstraints welcome = new GridBagConstraints();
         welcome.fill = GridBagConstraints.HORIZONTAL;
-        welcome.gridx =0;
-        welcome.gridy =0;
-        welcome.insets = new Insets(0,0,10,0);
+        welcome.gridx = 0;
+        welcome.gridy = 0;
+        welcome.insets = new Insets(0, 0, 10, 0);
         welcome.gridwidth = 2;
 
         GridBagConstraints nick = new GridBagConstraints();
         nick.fill = GridBagConstraints.HORIZONTAL;
-        nick.gridx =0;
-        nick.gridy =1;
+        nick.gridx = 0;
+        nick.gridy = 1;
 
         GridBagConstraints adrs = new GridBagConstraints();
         adrs.fill = GridBagConstraints.HORIZONTAL;
-        adrs.gridx =0;
-        adrs.gridy =2;
+        adrs.gridx = 0;
+        adrs.gridy = 2;
 
         GridBagConstraints nickbox = new GridBagConstraints();
         nickbox.fill = GridBagConstraints.HORIZONTAL;
-        nickbox.gridx =1;
-        nickbox.gridy =1;
-        nickbox.insets = new Insets(10,0,10,0);
+        nickbox.gridx = 1;
+        nickbox.gridy = 1;
+        nickbox.insets = new Insets(10, 0, 10, 0);
 
         GridBagConstraints adrsbox = new GridBagConstraints();
         adrsbox.fill = GridBagConstraints.HORIZONTAL;
-        adrsbox.gridx =1;
-        adrsbox.gridy =2;
-        welcome.insets = new Insets(10,0,10,0);
+        adrsbox.gridx = 1;
+        adrsbox.gridy = 2;
+        welcome.insets = new Insets(10, 0, 10, 0);
 
         GridBagConstraints button = new GridBagConstraints();
         button.fill = GridBagConstraints.HORIZONTAL;
-        button.gridx =0;
-        button.gridy =3;
-        button.insets = new Insets(30,0,0,0);
+        button.gridx = 0;
+        button.gridy = 3;
+        button.insets = new Insets(30, 0, 0, 0);
         button.gridwidth = 2;
 
         centerPanel.add(WelcomeLabel, welcome);
@@ -97,42 +97,75 @@ public class FirstFrame extends JFrame {
         FirstFrame.setVisible(true);
     }
 
-    public class EnterListener implements ActionListener, KeyListener
-    {
-
-        public void actionPerformed(ActionEvent event)
-        {
-            SetInformation();
-            sender.clientSocket = sender.make_connection();
-            sender.welcomeMessage(Nick);
-        }
-
-        public void keyPressed(KeyEvent event)
-        {
-            if(event.getKeyCode() == KeyEvent.VK_ENTER)
-            {
-                SetInformation();
-                sender.clientSocket = sender.make_connection();
-                sender.welcomeMessage(Nick);
-            }
-        }
-
-        public void keyReleased(KeyEvent e) {
-        }
-
-        public void keyTyped(KeyEvent e) {
-        }
-    }
-
-    private void SetInformation()
-    {
+    private void SetInformation() {
         if (NickBox.getText().length() >= 1) {
-            if(AddressBox.getText().length() >= 1) {
+            if (AddressBox.getText().length() >= 1) {
                 Nick = NickBox.getText();
                 Address = AddressBox.getText();
                 chatframe.getInformation(Nick, Address);
                 sender.getInformation(Nick, Address);
                 FirstFrame.setVisible(false);
+            }
+        }
+    }
+
+    public class MyInputVerifier {
+        public boolean verify(JComponent input) {
+            String text = ((JTextField) input).getText();
+            String[] parts = text.split("\\.");
+            if (parts.length != 4) {
+                errorMessage();
+                return false;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                if ((parts[i].length() > 3) || (parts[i].length() < 1))
+                {
+                    errorMessage();
+                    return false;
+                }
+            }
+            verified = true;
+            return true;
+        }
+    }
+
+    private void errorMessage() {
+        JOptionPane.showMessageDialog(null,
+                "Error: Wrong format of address", "Error Massage",
+                JOptionPane.ERROR_MESSAGE);
+    }
+
+    public class InputListener implements KeyListener, ActionListener {
+
+        public void keyTyped(KeyEvent ke) {
+            char ch = ke.getKeyChar();
+            if (!(Character.isDigit(ch)) && !(ch == '.')) ke.consume();
+        }
+
+        public void keyPressed(KeyEvent ke) {
+            if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
+                MyInputVerifier miv = new MyInputVerifier();
+                miv.verify(AddressBox);
+                if (verified) {
+                    SetInformation();
+                    sender.clientSocket = sender.make_connection();
+                    sender.welcomeMessage(Nick);
+                }
+
+            }
+        }
+
+        public void keyReleased(KeyEvent ke) {
+        }
+
+        public void actionPerformed(ActionEvent ke) {
+            MyInputVerifier miv = new MyInputVerifier();
+            miv.verify(AddressBox);
+            if (verified) {
+                SetInformation();
+                sender.clientSocket = sender.make_connection();
+                sender.welcomeMessage(Nick);
             }
         }
     }
