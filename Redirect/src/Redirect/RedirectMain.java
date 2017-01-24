a586b639aeb41680a705d6d932b2fe4d9343acdf
user <user@ASUS>
2017-01-24 11:03:50 +0100

    added comments to the rest

--- Redirect/src/Redirect/RedirectMain.java
+++ Redirect/src/Redirect/RedirectMain.java
@@ -10,11 +10,24 @@ public class RedirectMain {
     private static String version = "1.05";
     private final int PORT = 6789;
 
+    /**
+     * Creation of Redirect
+     * @param argv
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public static void main(String argv[]) throws IOException, InterruptedException {
         System.out.println("Redirect main " + version);
         RedirectMain redir = new RedirectMain();
         redir.run();
     }
+    
+    /**
+     * Creation of server socket
+     * Staring thread
+     * @throws InterruptedException
+     * @throws IOException 
+     */
 
     private void run() throws InterruptedException, IOException {
         Socket socket = null;
